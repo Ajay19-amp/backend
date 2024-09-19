c15a02ea63b625f87fc63005d079c71419945507
Ajay Kumar <ajaykumar19uk@gmail.com>
2024-09-20 01:29:58 +0530

    github data added

--- index.js
+++ index.js
@@ -2,12 +2,19 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = 4000
+const githubData= 
+{
+  "message": "Not Found",
+  "documentation_url": "https://docs.github.com/rest",
+  "status": "404"
+}
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 app.get('/twitter',(req,res)=>{
-    res.send('hiteshdotcom')
+    res.send('hello from twitter')
 })
 app.get('/login',(req,res)=>{
     res.send('<h1> please login </h1>')
@@ -15,7 +22,9 @@ app.get('/login',(req,res)=>{
 app.get('/youtube',(req,res)=>{
     res.send("<h2> hello youtube</h2>")
 })
-
+app.get('/github',(req,res)=>{
+  res.json(githubData)
+})
 
 
 app.listen(process.env.PORT, () => {
